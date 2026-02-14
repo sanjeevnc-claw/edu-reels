1e2e1646403c124d5c9e58c399f4c065306e266b
StitchClaw <stitch@stitchflow.com>
2026-02-14 16:01:20 +0000

    Fix: Update remotion.config.ts for v4 API

--- packages/remotion/remotion.config.ts
+++ packages/remotion/remotion.config.ts
@@ -1,14 +1,4 @@
 import { Config } from '@remotion/cli/config';
 
-// Enable webpack caching for faster builds
-Config.setWebpackCaching(true);
-
 // Set the entry point
 Config.setEntryPoint('./src/index.ts');
-
-// Output settings
-Config.setCodec('h264');
-Config.setPixelFormat('yuv420p');
-
-// Enable audio
-Config.setMuted(false);
